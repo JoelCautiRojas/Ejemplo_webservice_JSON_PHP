61f829fb4df65915cc6803ab7021a97afd96819d
JoelCautiRojas <joel.cauti@gmail.com>
2018-04-10 13:20:03 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+	
+	$responseData = array(
+		'status' 	=> 'success', 
+		'data' 		=> array(
+			'alumno1' => array(
+				'nombre' 	=> 'Jorge',
+				'telefono' => '985623564'
+			),
+			'alumno2' => array(
+				'nombre' 	=> 'Manuel',
+				'telefono' => '985627864'
+			),
+			'alumno3' => array(
+				'nombre' 	=> 'Ramiro',
+				'telefono' => '986213254'
+			),
+			'alumno4' => array(
+				'nombre' 	=> 'Jesus',
+				'telefono' => '986218956'
+			),
+			'alumno5' => array(
+				'nombre' 	=> 'Martin',
+				'telefono' => '945464578'
+			)
+		)
+	);
+
+	var_dump($responseData);
+
+
+ ?>
